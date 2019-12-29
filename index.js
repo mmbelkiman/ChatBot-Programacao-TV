fd35f0dfaca5de9a4b7e42e92a99965fe51d1a17
Marcelo Belkiman <marcelobelkiman@gmail.com>
2019-12-29 17:17:15 -0300

    add example img

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fetch = require('node-fetch');
+const http = require('http');
 const restService = express();
 
 restService.use(
